9300e49aca969e11fa44bf14c69423487bfe2f06
anish-pratheepkumar <anish.pratheepkumar@gmail.com>
2020-03-22 13:33:39 +0100

    Renamed weights as param in Optimizer.py

--- DeepLearningCourseProfMüller/Exercise_5_SGDfromScratch/optimizer.py
+++ DeepLearningCourseProfMüller/Exercise_5_SGDfromScratch/optimizer.py
@@ -1,26 +1,25 @@
 class SGD:
     
     def __init__(self, params, lr=1e-3):
-        super().__init__()
+        #super().__init__()
         # TODO: save the input argument 'params' as list
         #       and store the learning rate
         self.params = list(params)                            #params is a generator, creating list from                                                               #params so that we can access data 
         self.lr = lr
-        pass
-    
+            
     def step(self):
         # TODO: update the parameters' gradients according to
         #       gradient descent with step size lr;
         #       for modifying the parameters use the function
         #       'copy_' on the parameters' 'data' attributes
-        for weights in self.params:
-            update = self.lr*weights.grad.data               #gradient descent update
-            weights.data.copy_(weights.data-update)          #using copy fn to update the weigts 
-        pass
-    
+        for param in self.params:
+            update = self.lr*param.grad.data               #gradient descent update
+            param.data.copy_(param.data-update)          #using copy fn to update the weigts 
+            
     def zero_grad(self):
         # TODO: set all the parameters' gradients to zero
         #       by calling 'zero_' on the parameters' gradients
-        for weights in self.params:
-            weights.grad.zero_()
-        pass
+        for param in self.params:
+            param.grad.zero_()
+            #param.grad.data = torch.zeros_like(param.grad.data)
+        
