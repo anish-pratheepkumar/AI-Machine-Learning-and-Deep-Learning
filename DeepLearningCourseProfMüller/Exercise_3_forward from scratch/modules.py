bc09c9f161d6e080459d349e33c15486baa3910b
anish-pratheepkumar <anish.pratheepkumar@gmail.com>
2020-03-22 12:26:01 +0100

    Updated CrossEntropy Loss with target

--- DeepLearningCourseProfMüller/Exercise_3_forward from scratch/modules.py
+++ DeepLearningCourseProfMüller/Exercise_3_forward from scratch/modules.py
@@ -12,17 +12,14 @@ class Network(Module):
     def __init__(self, layers=[]):
         # store the layers passed in the constructor in your Network object
         self.layers = layers
-        pass
-    
+            
     def forward(self, x):
         # for executing the forward pass, run the forward passes of each
         # single layer and pass the output as input to the next layer
         for i in range(len(self.layers)):
             x = self.layers[i].forward(x)
         return x
-        pass
-    
-    
+            
     def add_layer(self, layer):
         # append layer at the end of the already existing layer
         self.layers.append(layer)
@@ -35,48 +32,42 @@ class LinearLayer(Module):
         # store parameters W and b
         self.weights = W
         self.bias = b
-        pass
-    
+            
     def forward(self, x):
         # compute the linear transformation x -> Wx + b
-        return np.dot(self.weights,x) + self.bias
-        pass
-
+        return self.weights @ x + self.bias
+       
     
 class Sigmoid(Module):
     
     def forward(self, x):
         # implement the sigmoid
         return np.exp(x) / (1 + np.exp(x))
-        pass
-
+        
     
 class ReLU(Module):
     
     def forward(self, x):
         # implement a ReLU
         return np.maximum(x,0)
-        pass
-
-    
+        
+        
 class Loss(Module):
     
     def forward(self, prediction, target):
         pass
-
 
 class MSE(Loss):
     
     def forward(self, prediction, target):
         # implement MSE loss
         return np.square(prediction - target).mean()
-        pass
-
-
+        
 class CrossEntropyLoss(Loss):
     
     def forward(self, prediction, target):
         # implement cross entropy loss
-        softmax = np.exp(prediction)/np.sum(np.exp(prediction))
+        prediction = prediction - prediction.max()
+        softmax = np.exp(prediction[target])/np.sum(np.exp(prediction))
         return -np.log(softmax)
-        pass
+       
